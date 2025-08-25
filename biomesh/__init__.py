6f66a4adf265709138cc0ec175b3a09f4fc16e5a
Amadeus Gebauer <amadeus.gebauer@tum.de>
2025-08-25 16:36:13 +0200

    Implement simple Laplace solver with grad

--- biomesh/__init__.py
+++ biomesh/__init__.py
@@ -19,6 +19,8 @@ import meshio
 from .reorder import reorder
 from .adapt import lin_to_quad
 
+from . import laplace
+
 
 def combine_colored_stl_files(*stl_files: pathlib.Path) -> meshio.Mesh:
     """Combine multiple colored STL files into a single mesh that is
